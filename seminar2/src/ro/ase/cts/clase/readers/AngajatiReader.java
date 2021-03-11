6da5afe8e24a4127526d3cc83aab22bf4abb040e
ganchevaanelia <ganchevaanelia18@stud.ase.ro>
2021-03-11 18:05:41 +0200

    seminar 3 nachalo

--- seminar2/src/ro/ase/cts/clase/readers/AngajatiReader.java
+++ seminar2/src/ro/ase/cts/clase/readers/AngajatiReader.java
@@ -5,38 +5,31 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 import ro.ase.cts.clase.Angajat;
 import ro.ase.cts.clase.Aplicant;
 
-public class AngajatiReader extends IReader{
+public class AngajatiReader extends IReader {
 
+	public AngajatiReader(String filePath) {
+		super(filePath);
+	}
 
-    public AngajatiReader(String filePath) {
-        super(filePath);
-    }
+	public List<Aplicant> readAplicants() throws FileNotFoundException, NumberFormatException {
+		Scanner input2 = new Scanner(new File(super.filePath));
+		input2.useDelimiter(",");
+		List<Aplicant> angajati = new ArrayList<>();
 
-
-    public List<Aplicant> readAplicants() throws FileNotFoundException, NumberFormatException {
-        Scanner input2 = new Scanner(new File(super.filePath));
-        input2.useDelimiter(",");
-        List<Aplicant> angajati = new ArrayList<>();
-
-        while (input2.hasNext()) {
-            String nume = input2.next();
-            String prenume = input2.next();
-            int varsta = input2.nextInt();
-            int punctaj = input2.nextInt();
-            int nr = input2.nextInt();
-            String[] vect = new String[5];
-            for (int i = 0; i < nr; i++)
-                vect[i] = input2.next();
-            int salariu = input2.nextInt();
-            String ocupatie = input2.next();
-            Angajat a = new Angajat(nume, prenume, varsta, punctaj, nr, vect, salariu, ocupatie);
-            angajati.add(a);
-        }
-        input2.close();
-        return angajati;
-    }
+		while (input2.hasNext()) {
+			Angajat angajat = new Angajat();
+			super.citireAplicant(input2, angajat);
+			
+			int salariu = input2.nextInt();
+			String ocupatie = input2.next();
+			angajat.setSalariu(salariu);
+			angajat.setOcupatie(ocupatie);
+			angajati.add(angajat);
+		}
+		input2.close();
+		return angajati;
+	}
 }
